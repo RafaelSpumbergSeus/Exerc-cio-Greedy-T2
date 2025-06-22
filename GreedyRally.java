480ba3f901c5897858bc26c8d287517269a26bc8
Rafael Spumberg <rafaelspumberg31@gmail.com>
2025-06-22 20:20:49 -0300

    trab.final

--- GreedyRally.java
+++ GreedyRally.java
@@ -1,54 +1,91 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class GreedyRally {
-
-    // Método que retorna a lista de paradas escolhidas e conta iterações
     public static List<Integer> calcularParadas(int L, int d, List<Integer> paradas, int[] iteracoes) {
-        paradas.add(L);  // Adiciona o final como parada
-        List<Integer> paradasEscolhidas = new ArrayList<>();
+       if (L <= 0 || d <= 0 || paradas == null) {
+            throw new IllegalArgumentException("Parâmetros inválidos");
+        }
 
+        List<Integer> paradasOrdenadas = new ArrayList<>(paradas);
+        
+        Collections.sort(paradasOrdenadas);
+        
+        paradasOrdenadas.add(L);
+        
+        List<Integer> paradasEscolhidas = new ArrayList<>();
         int posicaoAtual = 0;
         int i = 0;
-        int n = paradas.size();
+        int n = paradasOrdenadas.size();
 
         while (i < n) {
             iteracoes[0]++;  
 
             int proximaParada = i;
-            // Avança até o ponto mais distante possível sem ultrapassar 'd'
-            while (proximaParada < n && paradas.get(proximaParada) - posicaoAtual <= d) {
+            while (proximaParada < n && paradasOrdenadas.get(proximaParada) - posicaoAtual <= d) {
                 proximaParada++;
             }
 
-            if (proximaParada - 1 == i - 1) {
-                System.out.println("Não é possível chegar ao final com as paradas fornecidas.");
+            if (proximaParada == i) {
+                System.err.println("Não é possível chegar ao ponto " + paradasOrdenadas.get(i) + 
+                                 " a partir da posição atual " + posicaoAtual + 
+                                 " com distância máxima diária de " + d);
                 return null;
             }
 
-            int paradaEscolhida = paradas.get(proximaParada - 1);
+            int paradaEscolhida = paradasOrdenadas.get(proximaParada - 1);
             paradasEscolhidas.add(paradaEscolhida);
             posicaoAtual = paradaEscolhida;
 
+            if (posicaoAtual == L) {
+                break;
+            }
+
             i = proximaParada;
+        }
+
+        if (posicaoAtual != L) {
+            System.err.println("Não foi possível completar o percurso com as paradas fornecidas.");
+            return null;
         }
 
         return paradasEscolhidas;
     }
 
     public static void main(String[] args) {
+        // Caso de teste 1: Caso normal
         int L = 25;
         int d = 10;
         List<Integer> paradas = new ArrayList<>();
+        
+       //Caso de teste 1: Paradas ordenadas
+        /* 
         paradas.add(4);
         paradas.add(9);
         paradas.add(13);
         paradas.add(18);
         paradas.add(22);
         paradas.add(24);
-        paradas.add(26);  
-        paradas.add(30);  
-        //paradas.add(45);  
+        */
+
+        // Caso de teste 2: Paradas não ordenadas
+        /* 
+        System.out.println("Teste com paradas não ordenadas:");
+        paradas.add(18);
+        paradas.add(4);
+        paradas.add(24);
+        paradas.add(13);
+        paradas.add(9);
+        paradas.add(22);
+        */
+
+        // Caso de teste 3: Caso impossível 
+        System.out.println("Teste com caso impossível:");
+        paradas.add(4);
+        paradas.add(9);
+        paradas.add(13);
+        paradas.add(30); // Ponto inalcançável
 
         int[] iteracoes = {0};  
 
@@ -71,4 +108,3 @@ public class GreedyRally {
         }
     }
 }
-
